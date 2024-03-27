3f42dbc8f3b5150552d44179abfa2a0075c64fde
xin <xin.chen@ideatek.tech>
2024-03-27 10:05:34 +0800

    zlog init

---
+++ zlog/handler.go
@@ -0,0 +1,34 @@
+package zlog
+
+import "fmt"
+
+func (h *Handler) Log(level int, msg interface{}) {
+	switch level {
+	case Level_Debug:
+		h.Debug(msg)
+	case Level_Info:
+		h.Info(msg)
+	case Level_Warn:
+		h.Warn(msg)
+	case Level_Error:
+		h.Error(msg)
+	default:
+		h.Debug(msg)
+	}
+}
+
+func (h *Handler) Debug(msg interface{}) {
+	fmt.Println("Debug", msg)
+}
+
+func (h *Handler) Info(msg interface{}) {
+	fmt.Println("Info", msg)
+}
+
+func (h *Handler) Warn(msg interface{}) {
+	fmt.Println("Warn", msg)
+}
+
+func (h *Handler) Error(msg interface{}) {
+	fmt.Println("Error", msg)
+}
